edbd3bccd44a40b7442c5031613770810a56a2e5
Andrea Zucchini <azga84@gmail.com>
2016-10-18 08:33:20 +0200

    create test configuration with webpack

---
+++ config/webpack.test.js
@@ -0,0 +1,40 @@
+var helpers = require('./helpers');
+
+module.exports = {
+  devtool: 'inline-source-map',
+
+  resolve: {
+    extensions: ['', '.ts', '.js']
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.ts$/,
+        loaders: ['awesome-typescript-loader', 'angular2-template-loader']
+      },
+      {
+        test: /\.html$/,
+        loader: 'html'
+
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
+        loader: 'null'
+      },
+      {
+        test: /\.css$/,
+        exclude: helpers.root('src', 'app'),
+        // We probably don't need to load and process the application-wide
+        // styles files for unit tests and doing so would slow us down; we'll
+        // use the null loader for those CSS.
+        loader: 'null'
+      },
+      {
+        test: /\.css$/,
+        include: helpers.root('src', 'app'),
+        loader: 'raw'
+      }
+    ]
+  }
+};
