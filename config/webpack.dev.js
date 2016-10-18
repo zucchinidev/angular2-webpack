c5f8773bdc493ef4d862b37ec32bbac585e319f0
Andrea Zucchini <azga84@gmail.com>
2016-10-18 08:32:48 +0200

    create dev configuration for webpack

---
+++ config/webpack.dev.js
@@ -0,0 +1,28 @@
+var webpackMerge = require('webpack-merge');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var commonConfig = require('./webpack.common.js');
+var helpers = require('./helpers');
+
+module.exports = webpackMerge(commonConfig, {
+  devtool: 'cheap-module-eval-source-map',
+
+  output: {
+    // Although we tell Webpack to put output bundles
+    // in the dist folder, the dev server keeps all bundles in memory;
+    // it doesn't write them to disk. So we won't find any files in the
+    // dist folder (at least not any generated from this development build).
+    path: helpers.root('dist'),
+    publicPath: 'http://localhost:8080/',
+    filename: '[name].js',
+    chunkFilename: '[id].chunk.js'
+  },
+
+  plugins: [
+    new ExtractTextPlugin('[name].css')
+  ],
+
+  devServer: {
+    historyApiFallback: true,
+    stats: 'minimal'
+  }
+});
